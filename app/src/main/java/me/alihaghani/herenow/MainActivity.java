bdcae47538cbfd54ffd3f3e9e39bc604b5830ad0
Ali Haghani <alihaghani96@gmail.com>
2016-01-23 23:40:09 -0700

    MainActivity GeoFence stuff.

--- app/src/main/java/me/alihaghani/herenow/MainActivity.java
+++ app/src/main/java/me/alihaghani/herenow/MainActivity.java
@@ -1,10 +1,8 @@
 package me.alihaghani.herenow;
 
 import android.app.Fragment;
-<<<<<<< Updated upstream
-=======
+import android.app.IntentService;
 import android.app.PendingIntent;
->>>>>>> Stashed changes
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -19,19 +17,26 @@ import android.widget.AutoCompleteTextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.support.v4.app.ActivityCompat.startActivityForResult;
 
 public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
     GoogleApiClient mGoogleApiClient;
-    AutoCompleteAdapter mAdapter;
+    PendingIntent mGeofencePendingIntent;
+    private LocationServices mLocationService;
 
-<<<<<<< Updated upstream
-=======
     private PendingIntent getGeofencePendingIntent() {
         // Reuse the PendingIntent if we already have it.
+
+
         if (mGeofencePendingIntent != null) {
             return mGeofencePendingIntent;
         }
@@ -43,90 +48,98 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
 
     }
 
-
->>>>>>> Stashed changes
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-        //--Snippet
-        mGoogleApiClient = new GoogleApiClient
-                .Builder( this )
-                .enableAutoManage( this, 0, this )
-                .addApi( Places.GEO_DATA_API )
-                .addApi( Places.PLACE_DETECTION_API )
-                .addConnectionCallbacks( this )
-                .addOnConnectionFailedListener( this )
-                .build();
+    private PendingIntent getGeofenceTransitionPendingIntent() {
+        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if( mGoogleApiClient != null )
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_main);
+            //--Snippet
+            mGoogleApiClient = new GoogleApiClient
+                    .Builder(this)
+                    .enableAutoManage(this, 0, this)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+
+            super.onCreate(savedInstanceState);
             mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
-            mGoogleApiClient.disconnect();
         }
-        super.onStop();
 
-        if( mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
-            mAdapter.setGoogleApiClient( null );
-            mGoogleApiClient.disconnect();
+        @Override
+        protected void onStart() {
+            super.onStart();
+            if (mGoogleApiClient != null)
+                mGoogleApiClient.connect();
         }
-        super.onStop();
 
+        @Override
+        protected void onStop() {
+            if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.disconnect();
+            }
+            super.onStop();
+
+            if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+//                mAdapter.setGoogleApiClient(null);
+                mGoogleApiClient.disconnect();
+            }
+            super.onStop();
+
+        }
+
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+
+        }
+
+        @Override
+        public void onConnected(Bundle bundle) {
+//            if (mAdapter != null)
+//                mAdapter.setGoogleApiClient(mGoogleApiClient);
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+
+
+        private static final int CONTACT_PICKER_RESULT = 1001;
+        private int RESULT_OK;
+        private String DEBUG_TAG;
+
+
+        public void doLaunchContactPicker(View view) {
+            Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                    ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
+        }
+
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+            startActivityForResult(intent, 1);
+
+            String phoneNo = null;
+            Uri uri = data.getData();
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            cursor.moveToFirst();
+
+            int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+            phoneNo = cursor.getString(phoneIndex);
+            Contacts.add(phoneNo);
+
+
+        }
     }
 
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        if( mAdapter != null )
-            mAdapter.setGoogleApiClient( mGoogleApiClient );
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-
-    private static final int CONTACT_PICKER_RESULT = 1001;
-    private int RESULT_OK;
-    private String DEBUG_TAG;
-
-
-
-    public void doLaunchContactPicker(View view) {
-        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
-                ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-        startActivityForResult(intent, 1);
-
-        String phoneNo = null;
-        Uri uri = data.getData();
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-
-        int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-        phoneNo = cursor.getString(phoneIndex);
-        Contacts.add(phoneNo);
-
-
-    }
-}
